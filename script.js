82dba5b71a612d6da87be4567f5f488d2a4cb49a
Isadora vargas <isadorasilveiradevragas6@gmail.com>
2023-04-05 22:02:30 -0300

    Iniciando com Js

---
+++ script.js
@@ -0,0 +1,31 @@
+// meu primeiro comentário 
+
+/*
+  segundo comentário 
+  ijhuihyyf 
+
+*/
+console.log ("olá mundo!");
+
+//prompt ("Qual sua idade?")
+
+
+//conhecendo e trabalhando com variáveis 
+
+const  diadenascimento = 26 
+let texto = "meu dia de nascimento é: "
+
+console.log(texto) 
+console.log(diadenascimento) 
+
+//concatenando varíaveis 
+console.log(texto + diadenascimento) 
+
+//exemplos 
+const primeironome = "isadora"
+const sobrenome ="vargas"
+let minhaidade  = 16;
+let souestudante = true; 
+
+//imprimir informções no console 
+console.log (" meu nome é:",primeironome, sobrenome, " minha idade é:",minhaidade, " sou estudante?",souestudante)
